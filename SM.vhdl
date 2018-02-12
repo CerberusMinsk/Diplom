161b53cf5c5633218048a9b31ba5ac8be35b8a77
Cerberus <cerberus.minsk@gmail.com>
2018-02-12 16:31:33 +0300

    delete all coments

--- SM.vhdl
+++ SM.vhdl
@@ -1,22 +1,4 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date:    10:13:09 02/09/2018 
--- Design Name: 
--- Module Name:    SM - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
-----------------------------------------------------------------------------------
 
 package matrix is
     Type matrix is array (natural range <>,
@@ -29,16 +11,7 @@ package body matrix is
 -----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
 use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 library work;
 use work.matrix.all;
@@ -51,8 +24,6 @@ entity SM is
 end SM;
 
 architecture Behavioral of SM is
-
-    --variable AA, BB, CC: matrix(1 to 2, 1 to 2); --?
 
     procedure mult (signal AA: in matrix(1 to 2, 1 to 2);
                     signal BB: in matrix(1 to 2, 1 to 2);
